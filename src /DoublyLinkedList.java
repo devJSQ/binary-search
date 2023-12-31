a8c692537bf49847b49ad5b53edc251368b6bc1c
lamabeta <148013077+lamabeta@users.noreply.github.com>
2023-12-31 10:20:23 +0300

    Create DoublyLinkedList.java

---
+++ src /DoublyLinkedList.java
@@ -0,0 +1,19 @@
+class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class DoublyLinkedList{
+   public static void linearSearch(Node head, int target) {
+          /*add your code here*/
+   }
+   
+   public static void main(String[] args) {
+        /* call  the function */  
+   }
+}
