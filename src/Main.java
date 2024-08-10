f4592e29685ce6e573493f8ba91d50671436e0d7
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-08-10 18:26:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main{
-   public static void main(String[] args) {
-        /*add your code here*/
-   }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        
+        int[] elements = {1, 3, 5, 7, 9, 11, 13, 15};
+        for (int element : elements) {
+            list.add(element);
+        }
+        
+        list.binarySearch(9); 
+    }
 }
